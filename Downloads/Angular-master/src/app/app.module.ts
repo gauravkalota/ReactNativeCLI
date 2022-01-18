16f24d80c56517b6fd03c1e676a3c206c3db15da
gauravkalota <gaurav@acmeminds.com>
2022-01-18 16:40:53 +0530

    18commit

---
+++ Downloads/Angular-master/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
+import{SharedService} from './shared.service';
+import { MatTableModule } from '@angular/material/table';
+import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    AddEditDepComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    NgxPaginationModule
+  ],
+  providers: [SharedService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Downloads/Angular-master/src/app/employee/show-emp/show-emp.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-emp',
+  templateUrl: './show-emp.component.html',
+  styleUrls: ['./show-emp.component.css']
+})
+export class ShowEmpComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  EmployeeList:any=[];
+
+  ModalTitle:string;
+  ActivateAddEditEmpComp:boolean=false;
+  emp:any;
+
+////Filtering
+EmployeeIdFilter:string="";
+EmployeeNameFilter:string="";
+EmployeeListWithoutFilter:any=[];
+page: number = 1;
+
+
+
+
+
+  ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  addClick(){
+    this.emp={
+      EmployeeId:0,
+      EmployeeName:"",
+      Department:"",
+      DateOfJoining:"",
+      PhotoFileName:"anonymous.png"
+    }
+    this.ModalTitle="Add Employee";
+    this.ActivateAddEditEmpComp=true;
+
+  }
+
+  editClick(item){
+    console.log(item);
+    this.emp=item;
+    this.ModalTitle="Edit Employee";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  deleteClick(item){
+    if(confirm('Are you sure??')){
+      this.service.deleteEmployee(item.EmployeeId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshEmpList();
+      })
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditEmpComp=false;
+    this.refreshEmpList();
+  }
+
+
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList=data;
+      this.EmployeeListWithoutFilter=data;
+    });
+  }
+
+  FilterFn(){
+    var EmployeeIdFilter = this.EmployeeIdFilter;
+    var EmployeeNameFilter = this.EmployeeNameFilter;
+
+    this.EmployeeList = this.EmployeeListWithoutFilter.filter(function (el){
+      return el.EmployeeId.toString().toLowerCase().includes(
+        EmployeeIdFilter.toString().trim().toLowerCase() 
+      ) &&
+      el.EmployeeName.toString().toLowerCase().includes(
+        EmployeeNameFilter.toString().trim().toLowerCase()
+      )
+    });
+  }
+  sortResult(prop,asc){
+    this.EmployeeList = this.EmployeeListWithoutFilter.sort(function(a,b){
+      if(asc){
+          return (a[prop]>b[prop])?1 : ((a[prop]<b[prop]) ?-1 :0);
+      }else{
+        return (b[prop]>a[prop])?1 : ((b[prop]<a[prop]) ?-1 :0);
+      }
+    })
+  }
+}
+   
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                               
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
